4e5a8ef4fb5d28a4125a54fcedf949545b92cf71
Momchil Bachvarov <momchilbuchvarov@abv.bg>
2018-09-17 22:39:33 +0300

    spring project init

---
+++ src/main/java/com/falcon/springboot/falcondemo/falcondemo/FalcondemoApplication.java
@@ -0,0 +1,12 @@
+package com.falcon.springboot.falcondemo.falcondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalcondemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FalcondemoApplication.class, args);
+	}
+}
